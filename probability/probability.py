0dd8c43100d66cb3e52f8a772f58d913025fdaa3
Yo Sato <yosato16@gmail.com>
2016-03-06 17:52:03 +0100

    probability stuff, deleting downstairs duplicate, plus changing mi stuff

--- probability/probability.py
+++ probability/probability.py
@@ -96,10 +96,6 @@ def rawfile2objs(RawFPWdPerLine,OutputFP=None):
     return myUGs,myBGStats
         
 
-def bit_required(Prob):
-    Bit= math.log(Prob,2)
-    return Bit
-
 def remove_adjust_distribution(OrgDist,Items2Remove):
     Prob2Remove=0
     DiscDist=copy.deepcopy(OrgDist)
@@ -118,6 +114,10 @@ def remove_adjust_distribution(OrgDist,Items2Remove):
 def check_sumzero(Dist):
     return sum(Dist.values())
 
+#======================
+#Entropy related funcs
+# heavily used!!!!
+#=====================
 
 def info_gain(Entropy1,Item,Dist):
     Entropy2=entropy(remove_adjust_distribution(Dist,[Item]))
@@ -128,50 +128,34 @@ def entropy(DiscDist):
     NEntropy=0
 #    DiscDist,Smoothed=SmoothedDiscDist
     for _EventName,Prob in DiscDist.items():
-        NEntropy=NEntropy+entropy_moto(Prob)
+        NEntropy=NEntropy+entropy_unit(Prob)
 #    (Cnt,Prob)=Smoothed
 #    Entropy=Entropy+(entropy_moto(Prob)*Cnt)
 
     return -NEntropy
 
+def entropy_unit(Prob):
+    return math.log(Prob,2)*Prob
+
 def mutual_info_unit(Marg1,Marg2,Joint):
-    return pw_mutual_info(Marg1,Marg2,Joint)*Joint
+    return pointwise_mutual_info(Marg1,Marg2,Joint)*Joint
 
 def all_mis(M1,M2,Joint):
     M1=float(M1); M2=float(M2); Joint=float(Joint)
     if Joint==1:
-        PWMI=MI=NPWMI=NMI=0
+        PMI=NPMI=MI=NMI=0
     else:
-        PWMI=pw_mutual_info(M1,M2,Joint)
-        NPWMI=PWMI/-bit_required(Joint)
+        PMI=pointwise_mutual_info(M1,M2,Joint)
+        NPMI=normalised_pmi(PMI,Joint)
         MI=mutual_info_unit(M1,M2,Joint)
-        NMI=MI/-bit_required(Joint)
-    return (MI,NMI),(PWMI,NPWMI)
+        NMI=MI/-math.log(Joint,2)
+    return (MI,NMI),(PMI,NPMI)
 
-def normalised_pwmi_mi(Marg1,Marg2,Joint):
-    PWMI=pw_mutual_info(Marg1,Marg2,Joint)
-    NPWMI=-(PWMI/bit_required(Joint))
-    NMIUnit=-(Joint/(Joint*bit_required(Joint)))
-    return NPWMI,NMIUnit
+def pointwise_mutual_info(Marg1,Marg2,Joint):
+    return math.log(Joint/(Marg1*Marg2),2)
 
-def normalised_mutual_info_unit(Marg1,Marg2,Joint):
-    return mutual_info_unit(Marg1,Marg2,Joint)/-entropy_moto(Joint)
-
-def entropy_moto(Prob):
-    return Prob*bit_required(Prob)
-
-def pw_mutual_info(Marg1,Marg2,Joint):
-    PWMI=bit_required(Joint/(Marg1*Marg2))
-    return PWMI
-
-def pw_mutual_info2(Marg1,Marg2,Cond2given1):
-    Joint=Cond2given1*Marg1
-    PWMI=bit_required(Joint/(Marg1*Marg2))
-    return PWMI
-
-def normalised_pwmi(Marg1,Marg2,Joint):
-    return pw_mutual_info(Marg1,Marg2,Joint)/-bit_required(Joint)
-
+def normalised_pmi(PMI,Joint):
+    return PMI/-math.log(Joint,2)
 
 def condprob_fromjoint(Joint,MargGiven):
     return Joint/MargGiven
