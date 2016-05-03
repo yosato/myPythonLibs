5b41a1de33932865c33074f04b9a414f7a5037ea
Yo Sato <yosato16@gmail.com>
2016-05-03 07:29:34 +0200

    normalisation yarikake

--- mecabtools/mecabtools.py
+++ mecabtools/mecabtools.py
@@ -29,10 +29,10 @@ def extract_samefeat_lines(FP,Colnums):
             RelvEls=tuple([ FtEls[Ind-1] for Ind in Colnums ])
             InfEls=tuple([ FtEls[Ind-1] for Ind in (5,6,7,8,9,11) ])
             #print(InfEls)
-            if InfEls[0]=='動詞' or InfEls[0]=='形容詞':
+#            if InfEls[0]=='動詞' or InfEls[0]=='形容詞':
                 #print(InfEls[0]+InfEls[5])
-                ContentsLinums[RelvEls].append(LiNe.strip())
-                SeenInf.add(InfEls)
+            ContentsLinums[RelvEls].append(LiNe.strip())
+            SeenInf.add(InfEls)
     ContentsLinums={ Fts:Lines for (Fts,Lines) in ContentsLinums.items() if len(Lines)>=2 }
     #ContentsLinums=sorted(ContentsLinums.items(),key=lambda x:len(x[1]),reverse=True)
 
@@ -40,16 +40,18 @@ def extract_samefeat_lines(FP,Colnums):
         sys.stderr.write('candidate\n')
         sys.stderr.write('\n'.join(Lines)+'\n')
         Clusters=cluster_homonyms([Line.split(',')[0] for Line in Lines])
-        for Cntr,Cluster in enumerate(Clusters):
+        for Cntr,ClusterPair in enumerate(Clusters):
             #RelvLines=[]
-            sys.stderr.write('\nresults'+str(Cntr+1)+'\n')
-            if len(Cluster)<=1:
-                sys.stderr.write('none\n')
-            else:
-                RelvLines=[ Line for Line in Lines if Line.split(',')[0] in Cluster ]
+            for KanaCluster,KanjiCluster in ClusterPair:
+                sys.stderr.write('\nresults'+str(Cntr+1)+'\n')
+                cluster_process(KanjiCluster)
+                def cluster_process(Cluster):
+                    for Group in Cluster:
+                        if len(Cluster)>=2:
+                            RelvLines=[ Line for Line in Lines if Line.split(',')[0] in Cluster ]
             
-                sys.stdout.write('\n'.join(RelvLines)+'\n\n')
-            
+                        sys.stdout.write('\n'.join(RelvLines)+'\n\n')
+            if 
             
                 
     return ContentsLinums
@@ -76,9 +78,9 @@ def homonympair_identical_p(Homonym1,Homonym2):
 def cluster_homonyms(Homs):
     #print(Homs)
     #print()
-    KanaOnlys={ Hom  for Hom in Homs if myModule.all_of_chartypes_p(Hom,['hiragana','katakana']) }
+    KanaOnlys=[ Hom  for Hom in Homs if myModule.all_of_chartypes_p(Hom,['hiragana','katakana']) ]
     Prv=None;Clusters=[set()]
-    for Cntr,Hom in enumerate(set(Homs)-KanaOnlys):
+    for Cntr,Hom in enumerate(set(Homs)-set(KanaOnlys)):
         if Cntr==0:
             Clusters[-1].add(Hom)
         else:
@@ -87,7 +89,7 @@ def cluster_homonyms(Homs):
             else:
                 Clusters.append({Hom})
         Prv=Hom
-    return [ Cluster.union(KanaOnlys) for Cluster in Clusters ]
+    return ([KanaOnlys],Clusters) 
 
 
         
