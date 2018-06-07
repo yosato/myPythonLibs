3db2794562359baeec677c8d85e07db16d811936
Yo Sato <yosato16@gmail.com>
2018-06-07 06:28:13 +0100

    aaa

---
+++ mecabtools/simple_translate.py
@@ -0,0 +1,15 @@
+import os,sys
+import mecabtools
+
+def main0(SrcFP,TgtFP):
+    mecabtools.
+
+def main():
+    import argparse
+    Psr=argparse.ArgmentParser()
+    Psr.add_argument(srcfp)
+    Psr.add_argument(tgtfp)
+    Args=Psr.parse_args()
+    main0(Args.srcfp,Args.tgtfp)
+if __name__=='__main__':
+    main()
